ec5079262dd5dfc679e89cbc076d7287570782d2
fabiobatalha <fabiobatalha@gmail.com>
2016-08-10 11:22:04 -0300

    Compatibilidade com Python2 e Python3

--- scieloh5m5/h5m5.py
+++ scieloh5m5/h5m5.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 import os
-import codecs
 import csv
 import logging
 
@@ -10,10 +9,9 @@ _CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
 JOURNALS = {}
 
 
-with codecs.open(_CURRENT_DIR + '/data/google_metrics_h5m5.csv', 'r') as metrics:
+with open(_CURRENT_DIR + '/data/google_metrics_h5m5.csv', 'r') as metrics:
     spamreader = csv.reader(metrics, delimiter=',', quotechar='"')
     for line in spamreader:
-        line = [i.decode('utf-8') for i in line]
         issn = line[0].upper()
         year = line[1]
         h5 = line[3]
